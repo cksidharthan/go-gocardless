d22f6db89e52cd909bee75d07b9ebdcd59d2143b
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2023-05-06 10:20:56 +0200

    feat: add requisitions post endpoint

--- endpoints/accounts/accounts_endpoints.go
+++ endpoints/accounts/accounts_endpoints.go
@@ -9,5 +9,8 @@ import (
 func (a Client) Get(ctx context.Context, accountID string) (account *Account, err error) {
 	path := fmt.Sprintf("/accounts/%s", accountID)
 	err = a.HTTP.Get(ctx, path, nil, account)
+	if err != nil {
+		return nil, err
+	}
 	return account, nil
 }
---
+++ endpoints/requisitions/requisitions_endpoints.go
@@ -0,0 +1,18 @@
+package requisitions
+
+import (
+	"context"
+
+	"github.com/weportfolio/go-nordigen/consts"
+)
+
+// Post creates a new requisition
+func (c Client) Post(ctx context.Context, token string, requisitionRequestBody *RequisitionRequestBody) (*Requisition, error) {
+	var requisition Requisition
+	err := c.HTTP.Post(ctx, consts.RequisitionsPath, consts.RequestHeadersWithAuth(token), requisitionRequestBody, &requisition)
+	if err != nil {
+		return nil, err
+	}
+
+	return &requisition, nil
+}
